52b8dce1e78aa783c2f1b4f61c730ddee99e6982
Fernando Lima <fernando@umavida.com>
2025-02-07 08:02:39 -0300

    Inclusão novos testes

---
+++ TestProject/UnitTest/Domain/AzureBlobStorageServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.UnitTest.Domain
+{
+ 
+    //TODO: implementar :: sem modelo
+    internal class AzureBlobStorageServiceTest
+    {
+    }
+}
